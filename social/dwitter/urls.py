96a0089e81e7b847a117eb933c83b3bfe8eaa411
Ammar-theScorpion <assiammar692@gmail.com>
2022-12-03 14:49:17 +0200

    df

--- social/dwitter/urls.py
+++ social/dwitter/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import dashboard, profile_list, profile
+from .views import dashboard, profile_list, profile, login_page, logout_user,registerPage
 app_name = "dwitter"
 urlpatterns = [
     path("", dashboard, name="dashboard"),
+    path("login/", login_page, name="login_registor"),
+    path("logout/", logout_user, name="logout_user"),
+    path("registor/", registerPage, name="registerPage"),
     path("profile_list/", profile_list, name="profile_list"),
     path("profile/<int:pk>", profile, name="profile"),
 ]
--- social/dwitter/views.py
+++ social/dwitter/views.py
@@ -1,7 +1,58 @@
 from django.shortcuts import render, redirect
+from  django.contrib import messages
 from .models import Profile, Dweet
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .forms import DweetForm
+from django.contrib.auth.decorators import login_required
 
+def logout_user(request):
+    logout(request)
+    return redirect ('dwitter:login_registor')
+
+def registerPage(request):
+    form = UserCreationForm()
+    contex = {'form':form}
+
+    if request.method == "POST":
+        f = UserCreationForm(request.POST)
+        if f.is_valid():
+            user = f.save(commit=False)
+            user.username = user.username
+            user.save()
+            login(request, user)
+            return redirect('dwitter:dashboard')
+        else:
+            messages.error(request, "fix things")
+    return render(request, 'dwitter/login_registor.html', contex)
+    
+def login_page(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('dwitter:dashboard')
+
+    context = {'page':page}
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'user not found!')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('dwitter:dashboard')
+
+        else:
+            messages.error(request, 'password is not valid!')
+
+    return render(request, 'dwitter/login_registor.html', context)
+
+
+@login_required(login_url="dwitter:login_registor")
 def dashboard(request):
     form = DweetForm(request.POST or None)
     if request.method == "POST":
@@ -18,11 +69,13 @@ def dashboard(request):
     "dwitter/dashboard.html",
     {"form": form, "dweets": followed_dweets},
     )
-    
+
+@login_required(login_url="dwitter:login_registor")
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, "dwitter/profile_list.html", {"profiles": profiles})
-
+    
+@login_required(login_url="dwitter:login_registor")
 def profile(request, pk):
     profile = Profile.objects.get(pk=pk)
     if request.method == "POST":
