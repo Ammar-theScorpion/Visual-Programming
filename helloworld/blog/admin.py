eb1559c1112276815a3b4947d05a0e74cbbf56ea
Ammar-theScorpion <79399411+Ammar-theScorpion@users.noreply.github.com>
2023-02-24 08:28:35 +0200

    Delete helloworld directory

--- helloworld/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-class PostAdmin(admin.ModelAdmin):
-    pass
-class CategoryAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(Post, PostAdmin)
-admin.site.register(Category, CategoryAdmin)
--- helloworld/projects/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path 
-from . import views
-urlpatterns = [
-    path('', views.all_view, name='all_view'),
-    path('<int:pk>/', views.get_view, name='get_view'),
-]
