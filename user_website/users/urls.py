cd3cd5b50d1e29946cf8e009081da3547e164c7b
Ammar-theScorpion <79399411+Ammar-theScorpion@users.noreply.github.com>
2023-02-24 08:30:42 +0200

    Delete user_website directory

--- user_website/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from users.views import dashboard,register
-
-urlpatterns = [
-path("dashboard/", dashboard, name="dashboard"),
-path("accounts/", include("django.contrib.auth.urls")),
-path("register/", register, name="register"),
-]
